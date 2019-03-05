3e9175285007a37fcd6a7d5ce1aa99cfc29b5c78
stasulam <l.ambroziak@protonmail.com>
2019-03-05 22:32:09 +0100

    add: transfer learning class

--- tmle/models.py
+++ tmle/models.py
@@ -12,7 +12,20 @@ from sklearn.metrics import balanced_accuracy_score
 from .dataloaders import ImageFoldersDataset
 
 
-class TransferLearning:
+class TransferLearning(object):
+    # TODO(lukasz) inherit from transformers.CNNFeatures
+    def __init__(
+            self,
+            experiments_path: Optional[str] = None,
+            experiments_name: Optional[str] = None
+    ) -> None:
+
+        self.save_experiment = os.path.join(
+            experiments_path,
+            '.'.join([experiments_name, 'pth'])
+        )
+        self.metrics = None
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def train(
             self,
@@ -21,68 +34,55 @@ class TransferLearning:
             optimizer: torch.optim,
             train_dataset: ImageFoldersDataset,
             test_dataset: ImageFoldersDataset,
-            model_dir: Optional[str] = None,
-            model_name: Optional[str] = None,
             n_epochs: int = 25,
             batch_size: int = 32,
-            shuffle: bool = True
+            shuffle: bool = True,
+            *args, **kwargs
     ):
-        """
-
-        :param train_dataset:
-        :param test_dataset:
-        :param model_dir:
-        :param model_name:
-        :param n_epochs:
-        :param batch_size:
-        :param shuffle:
-        :return:
-        """
+        # TODO(lukasz): add scheduler for learning rate
         metrics = defaultdict(list)
-        best_accuracy_test = 0.
+        best_score_test = 0.
         for epoch in range(n_epochs):
             model.train()
-            running_loss = 0.0
+            running_loss = 0.
             for data_idx, data in enumerate(train_dataset.loader(
                 batch_size=batch_size,
                 shuffle=shuffle
+                # TODO(lukasz): add sampler for imbalanced dataset
             )):
                 inputs, labels = data
-                if torch.cuda.is_available():
-                    inputs = inputs.to(torch.device('cuda:0'))
-                    labels = labels.to(torch.device('cuda:0'))
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
                 optimizer.zero_grad()
+                model = model.to(self.device)
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
                 running_loss += loss.item()
+                # TODO(lukasz): add as argument
                 if data_idx % 100 == 0:
                     msg = '[%d, %5d] loss: %.3f'
                     print(msg % (epoch + 1, data_idx + 1, running_loss / 100))
-                    running_loss = 0.0
-            # TODO(lukasz): measure accuracy_train during training.
-            accuracy_train = self.score(model, train_dataset)
-            accuracy_test = self.score(model, test_dataset)
-            metrics['acc_train'].append(accuracy_train)
-            metrics['acc_test'].append(accuracy_test)
+                    running_loss = 0.
+            score_train = self.score(model, train_dataset)
+            score_test = self.score(model, test_dataset)
+            metrics['score_train'].append(score_train)
+            metrics['score_test'].append(score_test)
             msg = '[%d] train score: %.3f, test score: %.3f'
-            print(msg % (epoch + 1, accuracy_train, accuracy_test))
-            # save model (make sure that Google Colab do not destroy your results).
-            if accuracy_test > best_accuracy_test:
-                torch.save(
-                    model,
-                    os.path.join(model_dir, '.'.join([
-                        model_name + '_' + time.strftime('%Y%m%d%H%M', time.localtime(time.time())),
-                        'pth'])))
-                best_accuracy_test = accuracy_test
+            print(msg % (epoch + 1, score_train, score_test))
+            # save model (make sure that Google Colab do not destroy your results)
+            if score_test > best_score_test:
+                torch.save(model, self.save_experiment)
+                best_score_test = score_test
+        self.metrics = metrics
+        return self
 
-    def score(self, model, dataset: ImageFoldersDataset) -> float:
-        """
-
-        :param dataset:
-        :return:
-        """
+    def score(
+            self,
+            model: torchvision.models,
+            dataset: ImageFoldersDataset
+    ) -> float:
         with torch.no_grad():
             # remember that you must call `model.eval()` to set dropout and batch
             # normalization layers to evaluation mode before running the inference.
@@ -91,9 +91,9 @@ class TransferLearning:
             batch_idx = 0
             for data in dataset.loader(batch_size=32):
                 inputs, labels = data
-                if torch.cuda.is_available():
-                    inputs = inputs.to(torch.device('cuda:0'))
-                    labels = labels.to(torch.device('cuda:0'))
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+                model = model.to(self.device)
                 outputs = model(inputs)
                 _, pred = torch.max(outputs.data, 1)
                 batch_size = labels.size(0)
